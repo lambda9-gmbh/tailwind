9ef0dce1e5634e6a6edbe5a5525d8542e77ed1e6
Matthias Metzger <noobymatze@proton.me>
2025-03-10 16:00:36 +0100

    bump version 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 
 allprojects {
     val version = (property("version") as? String).let { version ->
-        if (version == null || version == "unspecified") "0.1.0"
+        if (version == null || version == "unspecified") "0.2.0"
         else version
     }
     setProperty("version", version)
